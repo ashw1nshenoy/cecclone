d9c15e48e64eea5c4d9f2287c255dd16bc61635a
Ashwin <ashwinshenoy20@gmail.com>
2023-11-06 12:39:47 +0530

    scroll reveal

---
+++ index.js
@@ -0,0 +1,28 @@
+ScrollReveal({
+    reset:true,
+    duration:2000,
+    delay:25,
+})
+ScrollReveal().reveal('.abtreveal , .moving',{delay:500,origin:'bottom',interval:200,distance:'45px'})
+ScrollReveal().reveal('.vision ',{delay:300,origin:'left',interval:200,distance:'45px'})
+ScrollReveal().reveal('.mission ',{delay:300,origin:'right',interval:200,distance:'45px'})
+ScrollReveal().reveal('.cs ',{delay:150,origin:'bottom',interval:100,distance:'45px'})
+ScrollReveal().reveal('.is ',{delay:150,origin:'bottom',interval:100,distance:'45px'})
+ScrollReveal().reveal('.ai ',{delay:150,origin:'top',interval:100,distance:'45px'})
+ScrollReveal().reveal('.csd ',{delay:150,origin:'top',interval:200,distance:'45px'})
+ScrollReveal().reveal('.csbs ',{delay:150,origin:'bottom',interval:200,distance:'45px'})
+ScrollReveal().reveal('.ec ',{delay:150,origin:'bottom',interval:200,distance:'45px'})
+ScrollReveal().reveal('.mech ',{delay:150,origin:'top',interval:200,distance:'45px'})
+ScrollReveal().reveal('.training ',{delay:300,origin:'top',interval:200,distance:'45px'})
+ScrollReveal().reveal('.offer ',{delay:700,origin:'bottom',interval:200,distance:'45px'})
+ScrollReveal().reveal('.recruiters ',{delay:700,origin:'top',interval:200,distance:'45px'})
+ScrollReveal().reveal('.placedno ',{delay:700,origin:'bottom',interval:200,distance:'45px'})
+ScrollReveal().reveal('.fac ',{delay:500,origin:'bottom',interval:200,distance:'45px'})
+ScrollReveal().reveal('.amen ',{delay:600,origin:'left',interval:200,distance:'45px'})
+ScrollReveal().reveal('.sup ',{delay:600,origin:'right',interval:200,distance:'45px'})
+ScrollReveal().reveal('.add ',{delay:500,origin:'top',interval:200,distance:'45px'})
+ScrollReveal().reveal('.box-2 ',{delay:600,origin:'left',interval:200,distance:'45px'})
+ScrollReveal().reveal('.enq ',{delay:500,origin:'bottom',interval:200,distance:'45px'})
+ScrollReveal().reveal('.submitt ',{delay:700,origin:'right',interval:200,distance:'45px'})
+
+
